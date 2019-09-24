68859e1f58339e43f736f2d0ba5057f378291782
vaututrice <52630750+vaututrice@users.noreply.github.com>
2019-09-24 00:34:00 -0400

    Update mascots.js

--- themes/ruri-dark/mascots.js
+++ themes/ruri-dark/mascots.js
@@ -1,1 +1,1 @@
-var mascotList = [ "ruri1.png", "ruri2.png", "ruri3.png" ];
+var mascotList = [ "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png", "16.png", ];
